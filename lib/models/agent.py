6e411ffff13f3442d510cadd68bd723bac0888f3
Buddha Lo <lo_977@yahoo.com>
2025-01-24 09:20:49 -0800

    add regex validation  on agent class

--- lib/models/agent.py
+++ lib/models/agent.py
@@ -1,15 +1,18 @@
-from models.__init__ import CONN,CURSOR
+# lib/models/department.py
+from __init__ import CURSOR, CONN
 import re
 
 
 
 class Agent:
     all = []
-    def __init__(self,name,email,phone,dre_num=None):
+    def __init__(self,name,email,phone,dre_num,id = None):
+        self.id = id
         self.name = name
         self.email = email
         self.phone = phone
         self.dre_num = dre_num
+
         Agent.all.append(self)
 
     @property
@@ -19,6 +22,8 @@ class Agent:
     def name(self,name):
         if isinstance(name,str) and len(name) > 0:
             self._name = name
+        else:
+            raise ValueError("Name must be non-empty strings")
 
     @property
     def email(self):
@@ -28,18 +33,29 @@ class Agent:
         email_re = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         if re.match(email_re,email) and len(email) > 0:
             self._email = email
+        else:
+            raise ValueError("Email must be in 'email@example.com' !")
     
     @property
     def phone(self):
         return self._phone
     @phone.setter
     def phone(self,phone):
-        if isinstance(phone,int) and len(phone) > 10:
+        phone_regex = r"^\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{4}$"
+        if re.match(phone_regex,phone) and len(phone) > 10:
             self._phone = phone
+        else:
+            raise ValueError("Invalid numbers")
     @property
     def dre_num(self):
         return self._dre_num
     @dre_num.setter
     def dre_num(self,dre_num):
-        if isinstance(dre_num,int) and len(dre_num) > 6:
+        if isinstance(dre_num,str) and len(dre_num )>= 6:
             self._dre_num = dre_num
+        else:
+            raise ValueError("Invalid Dre, Numbers!!!")
+agent1 = Agent("Rakesh","rakesh@emal.com","444-423-2345","123445")
+agent1.dre_num = "00000000"
+# 
+print(agent1.dre_num)
--- lib/models/property.py
+++ lib/models/property.py
@@ -12,6 +12,7 @@ class Property:
         self.description = description
         self.agent = agent
         Property.all.append(self)
+
     @property
     def address(self):
         return self._address
@@ -19,3 +20,25 @@ class Property:
     def address(self,address):
         if isinstance(address,str) and len(address) > 5:
             self._address = address
+        else:
+            raise ValueError("Address must be non-empty string")
+
+    @property
+    def price(self):
+        return self._price
+    @price.setter
+    def price(self,price):
+        if isinstance(price,float) and price > 0:
+            self._price = price
+        else:
+            raise ValueError("Price must be non-empty numbers")
+    @property
+    def size(self):
+        return self._size
+    @size.setter
+    def size(self,size):
+        if isinstance(size,int) and size > 0:
+            self._size = size
+        else:
+            raise ValueError("Size must be non-empty numbers")
+        
