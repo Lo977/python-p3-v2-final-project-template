43e6ad0ff6f64df270d35762df77db2bf142247f
Buddha Lo <lo_977@yahoo.com>
2025-02-03 13:19:45 -0800

    create save,create methods in Agent

--- lib/models/agent.py
+++ lib/models/agent.py
@@ -1,11 +1,29 @@
-
+from models.__init__ import CURSOR,CONN
 
 class Agent:
     all = {}
-    def __init__(self, name, email, phone, dre_num, id):
+    def __init__(self, name, email, phone, dre_num):
         self.id = id
         self.name = name
         self.email = email
         self.phone = phone
         self.dre_num = dre_num
         pass
+
+    def save(self):
+        sql = """
+            INSERT INTO agents(name, email, phone, dre_num)
+            VALUES (?, ?, ?, ?) 
+            """
+        CURSOR.execute(sql,(self.name, self.email, self.phone, self.dre_num))
+        CONN.commit()
+        
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+
+    @classmethod
+    def create(cls, name, email, phone, dre_num):
+        agent = cls(name, email, phone, dre_num)    
+        agent.save()
+        return agent
